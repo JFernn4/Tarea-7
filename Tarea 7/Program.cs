3332f46f1754d3d5e1e0478e055f41cfa0181489
Jimmy Quevedo <jfermamdoqdl@gmail.com>
2024-07-25 20:01:23 -0600

    Acceso global a la lista y función de media

--- Tarea 7/Program.cs
+++ Tarea 7/Program.cs
@@ -1,40 +1,49 @@
-﻿bool menu = true;
-int option;
-while (menu)
+﻿namespace Calculadora
 {
-    Console.Clear();
-    ShowMenu();
-    option = Convert.ToInt32(Console.ReadLine());
-    switch (option)
+    internal class Program
     {
-        case 1:
+        private static List<int> valuesList = new List<int>();
+        static void Main(string[] args)
+        {
+            bool menu = true;
+            int option;
+            while (menu)
             {
                 Console.Clear();
-                AskNumber();
-                Console.ReadKey();
-                break;
-            }
-        case 2:
-            {
-                Console.Clear();
-                menu = false;
-                break;
-            }
-        case 6:
-            {
-                Console.Clear();
-                AskNumber();
-                break;
-            }
-        case 0:
-            {
-                Console.Clear();
-                menu = false;
-                break;  
-            }
+                ShowMenu();
+                option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        {
+                            Console.Clear();
+                            AskNumber();
+                            Console.ReadKey();
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Media:"+(Mean()));
+                            Console.ReadKey();
+                            break;
+                        }
+                    case 6:
+                        {
+                            Console.Clear();
+                            AskNumber();
+                            break;
+                        }
+                    case 0:
+                        {
+                            Console.Clear();
+                            menu = false;
+                            break;  
+                        }
         
-    }
-}
+                }
+            }
+        }
 static void ShowMenu()
 {
     Console.WriteLine("1. Ingresar números.");
@@ -47,7 +56,6 @@ static void ShowMenu()
 }
 static void AskNumber()
 {
-    List<int> valuesList = new List<int>();
     Console.WriteLine("Ingrese su cantidad de números");
     int nNumber = Convert.ToInt32(Console.ReadLine());
     for (int i = 0; i < nNumber; i++)
@@ -57,6 +65,9 @@ static void AskNumber()
         valuesList.Add(values);
     }
 }
-
+static double Mean()
 {
+    return valuesList.Average();
+}
+}
 }
